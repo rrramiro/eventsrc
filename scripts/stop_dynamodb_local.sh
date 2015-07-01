ca29daf8cb5e5873fb832b98eb8a3c41404b9178
Brian McKenna <brian@brianmckenna.org>
2015-07-02 09:45:04 +1000

    Remove scripts - use 2.0.3 included ones

--- scripts/stop_dynamodb_local.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh -e
-
-SCRIPT_DIR=`dirname "$0"`
-PORT=${1:-8000}
-DYNAMO_DB_HOME="$SCRIPT_DIR/../../dynamodb/$PORT"
-
-cat $DYNAMO_DB_HOME/dynamodb_local.pid | xargs kill
-rm $DYNAMO_DB_HOME/dynamodb_local.pid
