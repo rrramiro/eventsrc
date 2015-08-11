15c9d78f5bf5d515ef83824845e04d3bd439dd31
Luciano Leggieri <lleggieri@atlassian.com>
2015-08-11 12:15:07 +1000

    - no need to run the Future if transforming to a Task

--- dynamodb/src/test/scala/io/atlassian/event/TaskTransformation.scala
+++ dynamodb/src/test/scala/io/atlassian/event/TaskTransformation.scala
@@ -3,7 +3,6 @@ package io.atlassian.event
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB
 import io.atlassian.aws.WrappedInvalidException
 import io.atlassian.aws.dynamodb._
-import DynamoDBAction._
 
 import scalaz.~>
 import scalaz.concurrent.Task
@@ -12,7 +11,8 @@ object TaskTransformation {
   def runner(client: AmazonDynamoDB): DynamoDBAction ~> Task =
     new (DynamoDBAction ~> Task) {
       def apply[A](a: DynamoDBAction[A]): Task[A] =
-        a.runAction(client).fold({ i => Task.fail(WrappedInvalidException.orUnderlying(i)) }, { a => Task.now(a) })
-
+        new Task(a.run(client).run.value.map {
+          _.leftMap(WrappedInvalidException.orUnderlying)
+        })
     }
 }
