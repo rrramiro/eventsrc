df6f36efda3e1e443fdfcc6ee1daadba857ca02b
Jed Wesley-Smith <jed@atlassian.com>
2016-09-05 15:26:48 +1000

    added InvariantFunctor instance for EventStorage

--- core/src/main/scala/io/atlassian/event/stream/Event.scala
+++ core/src/main/scala/io/atlassian/event/stream/Event.scala
@@ -2,38 +2,45 @@ package io.atlassian.event
 package stream
 
 import org.joda.time.DateTime
-import scalaz.{ Equal, Ordering }
+
+import scalaz.{ Equal, Functor, Ordering }
 import scalaz.syntax.equal._
 
 /**
  * Event wraps the event payload with common information (event id and time of the event)
  */
-case class Event[KK, S, E](id: EventId[KK, S], time: DateTime, operation: E) {
+case class Event[K, S, A](id: EventId[K, S], time: DateTime, operation: A) {
   // TODO: Monocle
-  def updateId[LL, T](f: EventId[KK, S] => EventId[LL, T]): Event[LL, T, E] =
+  def updateId[KK, SS](f: EventId[K, S] => EventId[KK, SS]): Event[KK, SS, A] =
     Event(f(id), time, operation)
 }
 
 object Event {
-  def next[KK, S: Sequence, E](key: KK, seq: Option[S], op: E): Event[KK, S, E] =
+  def next[K, S: Sequence, E](key: K, seq: Option[S], op: E): Event[K, S, E] =
     Event(EventId(key, seq.map { Sequence[S].next }.getOrElse { Sequence[S].first }), DateTime.now, op)
 
   def at[K, S, E](e: Event[K, S, E]): (S, DateTime) =
     (e.id.seq, e.time)
 
-  implicit def eventEqual[K: Equal, S: Equal, E: Equal]: Equal[Event[K, S, E]] =
-    new Equal[Event[K, S, E]] {
-      def equal(a1: Event[K, S, E], a2: Event[K, S, E]): Boolean =
+  implicit def eventEqual[K: Equal, S: Equal, A: Equal]: Equal[Event[K, S, A]] =
+    new Equal[Event[K, S, A]] {
+      def equal(a1: Event[K, S, A], a2: Event[K, S, A]): Boolean =
         a1.id === a2.id && a1.time == a2.time && a1.operation === a2.operation
     }
 
+  implicit def EventFunctor[K, S]: Functor[Event[K, S, ?]] =
+    new Functor[Event[K, S, ?]] {
+      override def map[A, B](ev: Event[K, S, A])(f: A => B): Event[K, S, B] =
+        Event(ev.id, ev.time, f(ev.operation))
+    }
+
   object syntax {
-    implicit class EventSyntax[KK, S, E](val e: Event[KK, S, E]) extends AnyVal {
+    implicit class EventSyntax[K, S, A](val e: Event[K, S, A]) extends AnyVal {
       def at: (S, DateTime) =
         Event.at(e)
 
-      def process[K, V](s: Snapshot[S, V])(f: Option[V] => PartialFunction[E, Option[V]]): Snapshot[S, V] =
-        f(s.value).applyOrElse(e.operation, { (_: E) => s.value }) match {
+      def process[K, V](s: Snapshot[S, V])(f: Option[V] => PartialFunction[A, Option[V]]): Snapshot[S, V] =
+        f(s.value).applyOrElse(e.operation, { (_: A) => s.value }) match {
           case None    => Snapshot.deleted.tupled(e.at)
           case Some(v) => Snapshot.value(v).tupled(e.at)
         }
