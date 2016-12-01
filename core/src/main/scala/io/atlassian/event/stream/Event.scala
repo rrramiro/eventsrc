1c0f9913500c2fdca20788280917a0428214a62c
Brian McKenna <bmckenna@atlassian.com>
2016-12-01 16:18:06 +1100

    Comment about dodgy side-effect in Event.next
    LEM-1352

--- core/src/main/scala/io/atlassian/event/stream/Event.scala
+++ core/src/main/scala/io/atlassian/event/stream/Event.scala
@@ -15,6 +15,7 @@ case class Event[KK, S, E](id: EventId[KK, S], time: DateTime, operation: E) {
 }
 
 object Event {
+  // TODO: Add Task
   def next[KK, S: Sequence, E](key: KK, seq: Option[S], op: E): Event[KK, S, E] =
     Event(EventId(key, seq.map { Sequence[S].next }.getOrElse { Sequence[S].first }), DateTime.now, op)
 
