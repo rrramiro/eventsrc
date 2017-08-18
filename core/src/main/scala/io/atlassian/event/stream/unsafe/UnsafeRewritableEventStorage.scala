c5fdaddf59f667eb931b04a2a0ffea0b88f99554
Gareth Sime <gsime@atlassian.com>
2017-08-18 15:52:55 +1000

    POR-2245: Add `mapKS` to the rewritable store

--- core/src/main/scala/io/atlassian/event/stream/unsafe/UnsafeRewritableEventStorage.scala
+++ core/src/main/scala/io/atlassian/event/stream/unsafe/UnsafeRewritableEventStorage.scala
@@ -2,7 +2,8 @@ package io.atlassian.event.stream.unsafe
 
 import io.atlassian.event.stream.{ Event, EventStreamError }
 
-import scalaz.\/
+import scalaz.{ Functor, \/ }
+import scalaz.syntax.functor._
 
 /**
  * A rewritable source of events.
@@ -10,7 +11,7 @@ import scalaz.\/
  * This should not be used in the general case. Rewriting events violates the immutability of the stream and introduces
  * significant complication.
  */
-trait UnsafeRewritableEventStorage[F[_], K, S, E] {
+trait UnsafeRewritableEventStorage[F[_], K, S, E] { self =>
   /**
    * Rewrite `oldEvent` to `newEvent`.
    *
@@ -20,4 +21,15 @@ trait UnsafeRewritableEventStorage[F[_], K, S, E] {
    *         through the container F.
    */
   def unsafeRewrite(oldEvent: Event[K, S, E], newEvent: Event[K, S, E]): F[EventStreamError \/ Event[K, S, E]]
+
+  // Invariant bifunctor
+  def mapKS[KK, SS](k: KK => K, kk: K => KK, s: SS => S, ss: S => SS)(implicit F: Functor[F]) =
+    // TODO: Monocle would clean this up a bit.
+    new UnsafeRewritableEventStorage[F, KK, SS, E] {
+      val updateKey: Event[K, S, E] => Event[KK, SS, E] =
+        _.bimap(kk, ss)
+
+      override def unsafeRewrite(oldEvent: Event[KK, SS, E], newEvent: Event[KK, SS, E]): F[\/[EventStreamError, Event[KK, SS, E]]] =
+        self.unsafeRewrite(oldEvent.bimap(k, s), newEvent.bimap(k, s)).map { _.map { updateKey } }
+    }
 }
