01a3b601fddd050501608f6657a47549d839f4d1
Gareth Sime <gsime@atlassian.com>
2017-08-22 09:30:12 +1000

    POR-2245: Use `TrieMap` rather than just `Map`

--- core/src/main/scala/io/atlassian/event/stream/unsafe/UnsafeRewritableInMemoryEventStorage.scala
+++ core/src/main/scala/io/atlassian/event/stream/unsafe/UnsafeRewritableInMemoryEventStorage.scala
@@ -3,7 +3,8 @@ package event
 package stream
 package unsafe
 
-import scalaz.{ -\/, NonEmptyList, \/, \/- }
+import scala.collection.concurrent.TrieMap
+import scalaz.{-\/, NonEmptyList, \/, \/-}
 import scalaz.concurrent.Task
 
 /**
@@ -12,16 +13,24 @@ import scalaz.concurrent.Task
 trait UnsafeRewritableInMemoryEventStorage[KK, S, E] extends UnsafeRewritableEventStorage[Task, KK, S, E]
 
 object UnsafeRewritableInMemoryEventStorage {
-  type Storage[KK, S, E] = Map[KK, List[Event[KK, S, E]]]
+  type Stream[KK, S, E] = List[Event[KK, S, E]]
+  type Storage[KK, S, E] = TrieMap[KK, Stream[KK, S, E]]
 
-  def apply[KK, S: Sequence, E](map: Storage[KK, S, E]) = Task.delay {
+  def apply[KK, S: Sequence, E](store: Storage[KK, S, E]) = Task.delay {
     new UnsafeRewritableInMemoryEventStorage[KK, S, E] {
       override def unsafeRewrite(oldEvent: Event[KK, S, E], newEvent: Event[KK, S, E]): Task[\/[EventStreamError, Event[KK, S, E]]] =
         Task.now {
-          (oldEvent, newEvent) match {
-            case (o, n) if o.id != n.id            => -\/(EventStreamError.reject(NonEmptyList(Reason("Events don't match"))))
-            case (o, _) if !map.contains(o.id.key) => -\/(EventStreamError.reject(NonEmptyList(Reason("No userbase stream with that Id"))))
-            case (o, n)                            => replaceOrError(map.get(n.id.key).get, newEvent).map(_ => newEvent)
+          store.synchronized {
+            (oldEvent, newEvent) match {
+              case (o, n) if o.id != n.id => -\/(EventStreamError.reject(NonEmptyList(Reason("Events don't match"))))
+              case (o, _) if !store.contains(o.id.key) => -\/(EventStreamError.reject(NonEmptyList(Reason("No userbase stream with that Id"))))
+              case (o, n) =>
+                val newStream = replaceOrError(store.get(n.id.key).get, newEvent)
+                if (newStream.isRight) {
+                  store += (o.id.key -> newStream.toEither.right.get)
+                }
+                newStream.map(_ => newEvent)
+            }
           }
         }
 
@@ -32,7 +41,7 @@ object UnsafeRewritableInMemoryEventStorage {
        * @param newEvent The event to sub in
        * @return The final list of events
        */
-      private def replaceOrError(events: List[Event[KK, S, E]], newEvent: Event[KK, S, E]): \/[EventStreamError, List[Event[KK, S, E]]] =
+      private def replaceOrError(events: Stream[KK, S, E], newEvent: Event[KK, S, E]): \/[EventStreamError, Stream[KK, S, E]] =
         events.span(_.id.seq == newEvent.id.seq) match {
           case (a, Nil) => -\/(EventStreamError.reject(NonEmptyList(Reason("Seq doesn't exist in userbase"))))
           case (a, b)   => \/-(a ++ (newEvent :: b.tail))
