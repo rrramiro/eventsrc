ea8387f992082c28d90cdf0f31aa978e108cbf9f
Gareth Sime <gsime@atlassian.com>
2017-08-22 12:55:38 +1000

    POR-2245: Test in-mem r/w store changes one event

--- core/src/test/scala/io/atlassian/event/stream/unsafe/UnsafeRewritableInMemoryEventStorageSpec.scala
+++ core/src/test/scala/io/atlassian/event/stream/unsafe/UnsafeRewritableInMemoryEventStorageSpec.scala
@@ -17,16 +17,29 @@ class UnsafeRewritableInMemoryEventStorageSpec extends ScalaCheckSpec with Disju
     s2"""
          This spec tests that events can be rewritten in memory
 
-         Can rewrite an event ${canRewriteASingleEvent[Boolean, Long, String]}
+         Can rewrite an event        ${canRewriteASingleEvent[Boolean, Long, String]}
+         Will only rewrite one event ${onlyRewritesOneEvent[Boolean, Long, String]}
     """
 
   def canRewriteASingleEvent[KK: Equal: Arbitrary, S: Sequence: Arbitrary, E: Equal: Arbitrary] =
     Prop.forAll { (oldTestEvent: TestEvent[KK, S, E], newOperation: E) =>
       val oldEvent = oldTestEvent.event
-      val storage = TrieMap[KK, List[Event[KK, S, E]]](oldEvent.id.key -> List(oldEvent))
       val newEvent = oldEvent.copy(operation = newOperation)
+      val storage = TrieMap[KK, List[Event[KK, S, E]]](oldEvent.id.key -> List(oldEvent))
       val result = UnsafeRewritableInMemoryEventStorage(storage).unsafeRewrite(oldEvent, newEvent).run
 
       result must be_\/-(newEvent) and (storage.get(oldEvent.id.key) must beSome(List(newEvent)))
+    }
+
+  def onlyRewritesOneEvent[KK: Equal: Arbitrary, S: Sequence: Arbitrary, E: Equal: Arbitrary] =
+    Prop.forAll { (op1: TestEvent[KK, S, E], op2: E, newOperation: E) =>
+      val event1 = op1.event
+      val event2 = op1.event.copy(id = EventId.next(op1.event.id), operation = op2)
+      val rewritten = op1.event.copy(operation = newOperation)
+
+      val storage = TrieMap[KK, List[Event[KK, S, E]]](event1.id.key -> List(event1, event2))
+      val result = UnsafeRewritableInMemoryEventStorage(storage).unsafeRewrite(event1, rewritten).run
+
+      result must be_\/-(rewritten) and (storage.get(event1.id.key) must beSome(List(rewritten, event2)))
     }
 }
